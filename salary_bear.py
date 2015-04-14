2a609e21ab1873765846bc54ac20687c6f91e81f
slobo <aslobodnik@gmail.com>
2015-04-13 22:27:47 -0500

    update gitignore add raw user data

--- salary_bear.py
+++ salary_bear.py
@@ -1,11 +1,25 @@
 from flask import render_template,request,redirect, url_for, Flask
 from slugify import slugify
 import language_check
-
+import pandas as pd
 
 app = Flask(__name__)
 
 tool = language_check.LanguageTool('en-US')
+
+df = pd.DataFrame.from_csv('data/combined_state_uni.csv')
+
+df.columns = ['organization','department','last_name','first_name','middle_name','title','salary','state']
+df['id'] = df.index
+
+df['salary'] = df['salary'].apply(lambda x: float(x.replace("$",'').replace(",","")) if type(x) == str else x)
+df['organization'] = df['organization'].apply(lambda x: x.title())
+df['first_name'] = df['first_name'].apply(lambda x: x.title())
+df['last_name'] = df['last_name'].apply(lambda x: str(x).title())
+df['middle_name'] = df['middle_name'].apply(lambda x: str(x).title())
+df['title'] = df['title'].apply(lambda x: str(x).title())
+
+
 
 person ={  'state':'FL'
             ,'organization': 'Department of Health'
@@ -33,13 +47,15 @@ person['url'] = '/' + person['state'] + '/' + slugify(person['organization']) + 
 
 people = [person,person_1]
 
+
+
 @app.route('/')
 def home():
     return render_template('home.html',search_nav='active')
 
 @app.route('/<state>/<organization>/<name>-<int:person_id>')
 def profile(state,organization,name,person_id):
-    person = people[person_id - 1]
+    person = df.iloc[person_id]
     profile_text = generate_profile_description(person)
     return render_template('profile.html',person=person,profile_text=profile_text)
 
@@ -56,7 +72,9 @@ def generate_profile_description(person):
         profile_text = language_check.correct(profile_text, matches)
     return profile_text
     
-
+def generate_url(person):
+    url = '/' + person['state'] + '/' + slugify(person['organization']) + '/' + slugify(person['first_name'] + ' ' + person['middle_name'] + ' ' + person['last_name']) + '-' + str(person['id'])
+    return url
 
 if __name__ == "__main__":
     app.run(debug=True)
